ca56514a96ce92a5d88259fbf5017be4a985a0fb
Sudharshan <solderneer@gmail.com>
2021-05-07 18:57:39 +0800

    feat: added loading indicators

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
-  <div id="home" class="wrapper bg-purple-50 min-h-screen p-4">
-    <nav class="p-4 border-b border-gray-200">
-      <ul class="flex flex-row items-center">
-        <img src="../assets/logo-transparent-dark.png" class="w-14 h-14"/>
-        <div class="text-secondary bg-purple-100 px-4 h-12 flex justify-center items-center rounded-full ml-auto mr-4">
-          🔥 {{ streak }}
-        </div>
-        <button class="w-12 h-12 rounded-lg text-secondary bg-purple-100"><i class="fas fa-sign-out-alt"></i></button>
-      </ul>
-    </nav>
-    
-    <DailyReviewCard :dailyReviewCards="this.dailyReviewCards" class="mt-4" />
-    <!-- Daily lesson section -->
-    <h1 class="font-display text-2xl text-secondary pl-4 mt-8 font-normal">Today's Lesson</h1> 
-    <DailyLessonCard class="mt-4" :todayLesson="this.todayLesson" /> 
-    <button class="bg-purple-200 w-full font-display font-light text-secondary py-2 px-6 rounded flex mt-4" @click="$router.push({ name: 'lessons' })">View previous lessons...</button>
-    <h1 class="font-display text-2xl text-secondary pl-4 mt-8 font-normal mb-2">Upcoming Tutorials</h1>
+  <div id="home" class="wrapper bg-purple-50 min-h-screen p-4 flex flex-col">
+      <nav class="p-4 border-b border-gray-200">
+        <ul class="flex flex-row items-center">
+          <img src="../assets/logo-transparent-dark.png" class="w-14 h-14"/>
+          <div class="text-secondary bg-purple-100 px-4 h-12 flex justify-center items-center rounded-full ml-auto mr-4">
+            🔥 {{ streak }}
+          </div>
+          <button class="w-12 h-12 rounded-lg text-secondary bg-purple-100"><i class="fas fa-sign-out-alt"></i></button>
+        </ul>
+      </nav>
+      
+    <div v-if="!loading">
+      <DailyReviewCard :dailyReviewCards="this.dailyReviewCards" class="mt-4" />
+      <!-- Daily lesson section -->
+      <h1 class="font-display text-2xl text-secondary pl-4 mt-8 font-normal">Today's Lesson</h1> 
+      <DailyLessonCard class="mt-4" :todayLesson="this.todayLesson" /> 
+      <button class="bg-purple-200 w-full font-display font-light text-secondary py-2 px-6 rounded flex mt-4" @click="$router.push({ name: 'lessons' })">View previous lessons...</button>
+      <h1 class="font-display text-2xl text-secondary pl-4 mt-8 font-normal mb-2">Upcoming Tutorials</h1>
 
-    <!-- Generating Tutorial List -->
-    <TutorialListElement v-for="tutorial in this.upcomingTutorials" :key="tutorial.id" :title="tutorial.title" :datetime="tutorial.scheduled_time" class="mt-2" />
+      <!-- Generating Tutorial List -->
+      <TutorialListElement v-for="tutorial in this.upcomingTutorials" :key="tutorial.id" :title="tutorial.title" :datetime="tutorial.scheduled_time" class="mt-2" />
+    </div>
+    <div v-else class="flex-grow flex flex-col justify-center items-center">
+      <MoonLoader class="self-center" color="#7938D8"/>
+    </div>
   </div>
 </template>
 
@@ -26,6 +31,8 @@
 import DailyReviewCard from '../components/DailyReviewCard.vue'
 import DailyLessonCard from '../components/DailyLessonCard.vue'
 import TutorialListElement from '../components/TutorialListElement.vue'
+import { MoonLoader } from '@saeris/vue-spinners'
+
 
 // Services
 import { getSelf } from '../services/LearnerService.js'
@@ -39,9 +46,11 @@ export default {
     DailyReviewCard,
     DailyLessonCard,
     TutorialListElement,
+    MoonLoader,
   },
   data: function () {
     return {
+      loading: true,
       token: "",
       learnerId: "",
       streak: 0,
@@ -51,6 +60,8 @@ export default {
     }
   },
   created: async function () {
+    this.loading = true
+
     // # Check if the JWT exists
     let token = localStorage.getItem("token")
     if(token == null) { 
@@ -72,6 +83,8 @@ export default {
 
     // Get the daily review if there is one
     this.dailyReviewCards = await getDailyReview(this.token)
+
+    this.loading = false
   },
 
 }
--- frontend/src/views/Lessons.vue
+++ frontend/src/views/Lessons.vue
@@ -7,23 +7,29 @@
       </ul>
     </nav>
     
-    <div v-for="lesson in lessons" :key="lesson.id" class="shadow-sm bg-white rounded-md flex flex-col py-6 my-3">
-      <h3 class="font-display text-lg font-medium text-text px-6">{{ lesson.title }}</h3>
-      <span class="font-body text-sm text-gray-400 px-6 pb-4">{{ getFormattedDate(lesson.scheduled_date) }}</span>
-      <!-- Image preview -->
-      <iframe class="w-full h-52" allowFullScreen="allowFullScreen" frameBorder="0"
-        :src="lesson.video_link">
-      </iframe>
-      <span class="font-body text-sm font-light text-text px-6 pt-4">
-        {{ lesson.description }}
-      </span>
-      
-      <button @click="reviewCards(lesson.id)" class="bg-primary hover:bg-secondary tracking-widest font-body text-xs text-medium text-white uppercase p-2 mx-6 mt-4 rounded">Review</button>
+    <div v-if="!loading">
+      <div v-for="lesson in lessons" :key="lesson.id" class="shadow-sm bg-white rounded-md flex flex-col py-6 my-3">
+        <h3 class="font-display text-lg font-medium text-text px-6">{{ lesson.title }}</h3>
+        <span class="font-body text-sm text-gray-400 px-6 pb-4">{{ getFormattedDate(lesson.scheduled_date) }}</span>
+        <!-- Image preview -->
+        <iframe class="w-full h-52" allowFullScreen="allowFullScreen" frameBorder="0"
+          :src="lesson.video_link">
+        </iframe>
+        <span class="font-body text-sm font-light text-text px-6 pt-4">
+          {{ lesson.description }}
+        </span>
+        
+        <button @click="reviewCards(lesson.id)" class="bg-primary hover:bg-secondary tracking-widest font-body text-xs text-medium text-white uppercase p-2 mx-6 mt-4 rounded">Review</button>
+      </div>
+    </div>
+    <div v-else class="flex-grow flex flex-col justify-center items-center">
+      <MoonLoader class="self-center" color="#7938D8"/>
     </div>
   </div> 
 </template>
 
 <script>
+import { MoonLoader } from '@saeris/vue-spinners'
 import { getLessonsPast, getLessonFlashcards } from '../services/LessonService.js'
 
 const options = {
@@ -34,13 +40,18 @@ const options = {
 
 export default {
   name: 'Lessons',
+  components: {
+    MoonLoader,
+  },
   data: function () {
     return {
+      loading: true,
       token: null,
       lessons: Array,
     }
   },
   created: async function () {
+    this.loading = true
     // # Check if the JWT exists
     let token = localStorage.getItem("token")
     if(token == null) { 
@@ -58,6 +69,8 @@ export default {
 
       return d2 - d1
     })
+
+    this.loading = false
   },
   methods: {
     goBack: function() {

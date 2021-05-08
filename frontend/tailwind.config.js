716d98eaa69436915815b590a016ec3defd6e1be
Sudharshan <solderneer@gmail.com>
2021-05-08 21:47:56 +0800

    feat: added dockerfiles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.html',
+    './src/**/*.vue',
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {

efa549168f4211ff5f68243902d0098118496ec6
Sudharshan <solderneer@gmail.com>
2021-05-26 10:58:00 +0800

    feat: updating database structure

--- backend/define.sql
+++ backend/define.sql
@@ -1,6 +1,8 @@
 -- clear up old stuff
 DROP TABLE IF EXISTS learner CASCADE;
 DROP TABLE IF EXISTS module CASCADE;
+DROP TABLE IF EXISTS cohort CASCADE;
+DROP TABLE IF EXISTS learner_cohort CASCADE;
 DROP TABLE IF EXISTS lesson CASCADE;
 DROP TABLE IF EXISTS tutorial CASCADE;
 DROP TABLE IF EXISTS flashcard CASCADE;
@@ -12,14 +14,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- learner table hols the data about the users
 CREATE TABLE learner (
-  learner_id uuid DEFAULT uuid_generate_v4 (),
   email VARCHAR UNIQUE NOT NULL,
   first_name VARCHAR,
   last_name VARCHAR,
   last_completed TIMESTAMPTZ DEFAULT NOW(),
   streak INT NOT NULL DEFAULT 0,
 
-  PRIMARY KEY (learner_id)
+  PRIMARY KEY (email)
 );
 
 -- module ID is codes like CS0001 etc, based on the university
@@ -31,13 +32,37 @@ CREATE TABLE module (
   PRIMARY KEY (module_id)
 );
 
+-- cohort_start_date is always a Monday
+-- weekly tutorial starts the week before the cohort_start_date with the module orientation session
+-- weekly_tutorial_day starts at 0 for Monday and 6 for Sunday
+CREATE TABLE cohort (
+  cohort_id uuid DEFAULT uuid_generate_v4 (),
+  module VARCHAR NOT NULL,
+  completed BOOL NOT NULL DEFAULT FALSE,
+  start_date DATE NOT NULL,
+  weekly_tutorial_day INT NOT NULL CHECK (weekly_tutorial_day >= 0 AND weekly_tutorial_day <=6),
+  weekly_tutorial_time TIMETZ NOT NULL
+);
+
+CREATE TABLE learner_cohort (
+  learner VARCHAR,
+  cohort uuid,
+
+  PRIMARY KEY (learner, cohort),
+
+  CONSTRAINT fk_learner
+    FOREIGN KEY (learner) REFERENCES(email),
+  CONSTRAINT fk_cohort
+    FOREIGN KEY (cohort) REFERENCES(cohort_id)
+);
+
 -- lesson table holds the lessons in a module
 CREATE TABLE lesson (
   lesson_id uuid DEFAULT uuid_generate_v4 (),
   title VARCHAR NOT NULL,
   description TEXT NOT NULL,
   video_link VARCHAR NOT NULL,
-  scheduled_date DATE NOT NULL,
+  date_offset INT NOT NULL,
   module VARCHAR NOT NULL,
 
   PRIMARY KEY (lesson_id),
@@ -45,12 +70,25 @@ CREATE TABLE lesson (
     FOREIGN KEY (module) REFERENCES module(module_id)
 );
 
+CREATE TABLE learner_lesson (
+  learner VARCHAR,
+  lesson uuid,
+  scheduled_date DATE NOT NULL,
+  completed bool,
+  
+  PRIMARY KEY (learner, lesson),
+  
+  CONSTRAINT fk_learner
+    FOREIGN KEY (learner) REFERENCES learner(email),
+  CONSTRAINT fk_lesson
+    FOREIGN KEY (lesson) REFERENCES lesson(lesson_id)
+);
+
 -- tutorial table holds the tutorials for a module
 CREATE TABLE tutorial (
-  tutorial_id uuid DEFAULT uuid_generate_v4 (),
+  tutorial_id SERIAL,
   title VARCHAR NOT NULL,
   description TEXT NOT NULL,
-  scheduled_datetime TIMESTAMPTZ NOT NULL,
   module VARCHAR NOT NULL,
 
   PRIMARY KEY (tutorial_id),
@@ -72,27 +110,14 @@ CREATE TABLE flashcard (
 
 -- Relationship between flashcard and learner
 CREATE TABLE learner_flashcard (
-  learner uuid,
+  learner VARCHAR,
   flashcard uuid,
   repeat INT DEFAULT 0 NOT NULL,
 
   PRIMARY KEY (learner, flashcard),
   
   CONSTRAINT fk_learner
-    FOREIGN KEY (learner) REFERENCES learner(learner_id),
+    FOREIGN KEY (learner) REFERENCES learner(email),
   CONSTRAINT fk_flashcard
     FOREIGN KEY (flashcard) REFERENCES flashcard(flashcard_id)
-);
-
-CREATE TABLE learner_lesson (
-  learner uuid,
-  lesson uuid,
-  completed bool,
-  
-  PRIMARY KEY (learner, lesson),
-  
-  CONSTRAINT fk_learner
-    FOREIGN KEY (learner) REFERENCES learner(learner_id),
-  CONSTRAINT fk_lesson
-    FOREIGN KEY (lesson) REFERENCES lesson(lesson_id)
 );

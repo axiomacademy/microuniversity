6e27d1f38128fdacd671db4e21ae37f9052e786c
Sudharshan <solderneer@gmail.com>
2021-07-14 20:20:18 +0800

    feat: moving to a prototype UI to show functionality

--- frontend/src/components/DailyReviewCard.vue
+++ frontend/src/components/DailyReviewCard.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="shadow-sm p-6 bg-white rounded-md w-full flex flex-col justify-start items-start" @click="startReview" :disabled="dailyReviewCards.length == 0">
-    <h5 class="font-display text-md">{{ today }}</h5>
-    <h1 class="font-display text-3xl text-secondary font-medium">Daily Review</h1>
+    <h5 class="font-body text-md">{{ today }}</h5>
+    <h1 class="font-body font-semibold text-3xl text-secondary font-medium">Daily Review</h1>
     <h6 v-if="this.remainingCards == 0" class="font-body text-sm mt-5">✅ You’ve completed today’s review</h6>
     <h6 v-else class="font-body text-sm mt-5">You have <span class="font-body font-bold text-sm">{{ remainingCards }}</span> cards left to review</h6>
   </button> 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,53 +1,31 @@
 <template>
-  <div id="home" class="wrapper bg-purple-50 min-h-screen p-4 flex flex-col pb-32 items-center">
-    <!-- Nav bar -->
-    <nav class="relative p-4 border-b border-purple-200 lg:w-6/12 w-full">
-      <ul class="flex flex-row items-center">
-        <img src="../assets/logo-transparent-dark.png" class="w-14 h-14"/>
-        <div class="text-secondary bg-purple-100 px-4 h-12 flex justify-center items-center rounded-full ml-auto mr-4">
-          🔥 {{ streak }}
-        </div>
-        <button @click="showMenu = !showMenu" class="w-12 h-12 rounded-lg text-secondary bg-purple-100"><i class="fas fa-bars"></i></button>
-      </ul>
+  <div id="home" class="wrapper bg-purple-50 min-h-screen flex flex-col pb-32 items-center" v-bind:class="{ 'game': exploreTabOpen }"> 
 
-      <!-- Hamburger menu -->
-    <div v-if="showMenu" class="absolute bottom-0 right-0 -mb-32 mr-4 bg-purple-100 shadow-sm flex w-8/12 md:w-4/12 rounded-md overflow-hidden">
-      <ul class="flex flex-col w-full">
-        <li class="text-sm px-4 py-3 text-text">Signed in as <span class="font-medium">{{ email }}</span></li>
-        <div class="bg-purple-200 w-full" style="height: 1px;"></div>
-        <button @click="$router.push({ name: 'profile' })" class="text-sm text-text py-2 text-left px-4 hover:bg-purple-200 focus:bg-purple-200">
-          Profile
-        </button>
-        <div class="bg-purple-200 w-full" style="height: 1px;"></div>
-        <button @click="logout" class="text-sm text-text py-2 text-left px-4 hover:bg-purple-200 focus:bg-purple-200">
-          Sign Out
-        </button>
-      </ul>
-    </div>
-    </nav>
-      
     <div v-if="!loading" class="lg:w-6/12 w-full">
-      <!-- Explore tab with modules, enrolled cohorts etc. -->
+
       <div v-if="openTab == 'Explore'" id="explore-tab">
-        <h1 class="font-display text-2xl text-secondary pl-4 mt-6">Explore Modules 🧠</h1> 
+        EXPLORE
       </div>
-
-      <!-- Learning tab with daily review, lessons and tutorials -->
-      <div v-if="openTab == 'Learn'" id="learn-tab">
-        <DailyReviewCard :dailyReviewCards="this.dailyReviewCards" class="mt-4" />
-        <!-- Daily lecture section -->
-        <h1 class="font-display text-2xl text-secondary pl-4 mt-8">Today's Lecture</h1> 
-        <DailyLectureCard class="mt-4" :todayLecture="this.todayLecture" :token="this.token" /> 
-        <button class="bg-purple-200 w-full font-display font-light text-secondary py-2 px-6 rounded flex mt-4">View previous lectures...</button>
-        <h1 class="font-display text-2xl text-secondary pl-4 mt-8 font-normal mb-2">Upcoming Tutorials</h1>
-
-        <!-- Generating Tutorial List -->
-        <div v-if="upcomingTutorials.length == 0" class="shadow-sm bg-white rounded-md flex flex-col justify-center items-center py-6 mt-4">
-          <img src="../assets/relax.png" class="w-24" />
-          <h3 class="font-display text-lg text-text font-medium pt-4">No upcoming tutorials</h3>
-          <h6 class="font-display text-md text-text">Looks like you can relax for a while!</h6>
+      
+      <div v-if="openTab == 'Challenges'" id="learn-tab" class="py-10">
+        <h1 class="text-3xl font-display font-bold max-w-0 px-10 text-secondary">Mining Activities</h1>
+        <div class="px-4 py-4 mt-6">
+          <DailyReviewCard :dailyReviewCards="dailyReviewCards" />
         </div>
-        <TutorialListElement v-else v-for="tutorial in upcomingTutorials" :key="tutorial.id" :title="tutorial.title" :datetime="tutorial.scheduled_time" class="mt-2" />
+        <h2 class="text-xl font-body font-bold mt-8 mb-2 px-10 text-text">Challenges</h2>
+        <h2 class="text-xl font-body font-bold mt-8 mb-2 px-10 text-text">Tutorials</h2>
+      </div>
+      
+      <div v-if="openTab == 'Learn'" id="learn-tab" class="py-10">
+        <h1 class="text-3xl font-display font-bold max-w-0 px-10 text-secondary">Gather Knowledge</h1>
+        <h2 class="text-xl font-body font-bold mt-6 px-10 text-text">Recommendations</h2>
+        <div class="overflow-x-auto flex flex-nowrap mt-4 pl-4 horizontal">
+          <LectureCard v-for="lecture in lectures" :key="lecture.title" :lecture="lecture" :token="token" class="lectureCard" />
+        </div>
+      </div>
+      
+      <div v-if="openTab == 'Me'" id="me-tab">
+        ME
       </div>
     </div>
 
@@ -57,28 +35,34 @@
     </div>
 
     <!-- Floating tab buttons -->
-    <div class="fixed inset-x-0 bottom-0 mb-8 bg-white shadow-lg mx-auto flex w-9/12 lg:w-4/12 h-16 rounded-full justify-around overflow-hidden">
-      <button class="w-6/12 font-display focus:outline-none" @click="setActiveTab('Explore')"
-        v-bind:class="{ 'bg-purple-100': exploreTabOpen, 'text-secondary': exploreTabOpen, 'text-text': exploreTabOpen}">
-        🌎 <span class="pl-1" v-bind:class="{ 'font-medium': exploreTabOpen }">Explore</span>
-      </button>
-      <div class="h-full bg-purple-200" style="width: 1px;"></div>
-      <button class="w-6/12 font-display focus:outline-none" @click="setActiveTab('Learn')" 
-        v-bind:class="{ 'bg-purple-100': learnTabOpen, 'text-secondary': learnTabOpen, 'text-text': learnTabOpen }">
-        📖 <span class="pl-1" v-bind:class="{ 'font-medium': learnTabOpen }">Learn</span>
-      </button>
+    <div class="fixed inset-x-0 bottom-0">
+      <div class="mb-6 bg-white shadow-lg mx-4 flex lg:w-4/12 h-16 rounded-lg justify-around overflow-hidden">
+        <button class="w-4/12 text-2xl focus:outline-none" @click="setActiveTab('Explore')"
+          v-bind:class="{ 'bg-purple-100': exploreTabOpen, 'text-secondary': exploreTabOpen }">
+          <i class="fas fa-globe-europe"></i> 
+        </button>
+        <button class="w-4/12 text-2xl focus:outline-none" @click="setActiveTab('Challenges')"
+          v-bind:class="{ 'bg-purple-100': challengesTabOpen, 'text-secondary': challengesTabOpen }">
+          <i class="fas fa-space-shuttle"></i>
+        </button>
+        <button class="w-4/12 text-2xl focus:outline-none" @click="setActiveTab('Learn')" 
+          v-bind:class="{ 'bg-purple-100': learnTabOpen, 'text-secondary': learnTabOpen }">
+          <i class="fas fa-graduation-cap"></i>
+        </button>
+        <button class="w-4/12 text-2xl focus:outline-none" @click="setActiveTab('Me')" 
+          v-bind:class="{ 'bg-purple-100': meTabOpen, 'text-secondary': meTabOpen }">
+          <i class="fas fa-user-astronaut"></i>
+        </button>
+      </div>   
     </div>
   </div>
 </template>
 
 <script>
-import DailyReviewCard from '../components/DailyReviewCard.vue'
-import DailyLectureCard from '../components/DailyLectureCard.vue'
-import TutorialListElement from '../components/TutorialListElement.vue'
 import { MoonLoader } from '@saeris/vue-spinners'
 
-
-// Services
+import LectureCard from '../components/LectureCard.vue'
+import DailyReviewCard from '../components/DailyReviewCard.vue'
 
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -86,24 +70,38 @@ import "firebase/auth";
 export default {
   name: 'App',
   components: {
-    DailyReviewCard,
-    DailyLectureCard,
-    TutorialListElement,
     MoonLoader,
+    LectureCard,
+    DailyReviewCard,
   },
   data: function () {
     return {
       loading: true,
       token: "",
       email: "",
-      streak: 0,
-      todayLecture: null,
-      upcomingTutorials: [],
-      dailyReviewCards: [],
       openTab: "Learn",
-      showMenu: false,
-      existingCohort: {},
       unsubAuth: null,
+      dailyReviewCards: [],
+      lectures: [
+        {
+          title: "Electronic Computing",
+          subject: "Computer Science",
+          description: "Learn all about electronic computing and be aware of the 1819 census",
+          video_link: "https://www.youtube.com/embed/LN0ucKNX0hc"
+        },
+        {
+          title: "Electronic Computing",
+          subject: "Computer Science",
+          description: "Learn all about electronic computing and be aware of the 1819 census",
+          video_link: "https://www.youtube.com/embed/LN0ucKNX0hc"
+        },
+        {
+          title: "Electronic Computing",
+          subject: "Computer Science",
+          description: "Learn all about electronic computing and be aware of the 1819 census",
+          video_link: "https://www.youtube.com/embed/LN0ucKNX0hc"
+        }
+      ],
     }
   },
   computed: {
@@ -112,6 +110,12 @@ export default {
     },
     exploreTabOpen: function () {
       return this.openTab == "Explore"
+    },
+    meTabOpen: function () {
+      return this.openTab == "Me"
+    },
+    challengesTabOpen: function () {
+      return this.openTab == "Challenges"
     },
   },
   created: async function () {
@@ -143,19 +147,7 @@ export default {
   },
   methods: {
     setActiveTab: async function(tab) {
-      if(tab == "Learn") {
-        this.openTab = "Learn"
-        this.loading = true
-        // Functions go here
-        this.loading = false
-      } else if (tab == "Explore") {
-        this.openTab = "Explore"
-        this.loading = true
-        // Functions go here
-        this.loading = false
-      } else {
-        return 
-      }
+      this.openTab = tab
     },
     logout: async function() {
       this.loading = true
@@ -166,5 +158,24 @@ export default {
 </script>
 
 <style>
+
+.game {
+  background-image: url("../assets/bg.jpeg");
+}
+
+.horizontal::-webkit-scrollbar {
+  display: none;
+}
+ 
+.horizontal {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;
+  scroll-snap-type: x mandatory;
+}
+
+.lectureCard {
+  scroll-snap-align: center;
+  min-width: 80vw;
+}
 
 </style>
